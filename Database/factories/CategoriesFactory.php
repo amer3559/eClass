c9e227d53168e5cce08f9491cde9609a916cf5e8
Amer_Morsy <amer.morsy2015@gmail.com>
2020-11-14 13:47:34 +0200

    first commit

---
+++ Database/factories/CategoriesFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+//  'title','icon','slug','featured','status', 'position'
+
+$factory->define(App\Categories::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ Database/seeds/LanguagesTableSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class LanguagesTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+
+        \DB::table('languages')->delete();
+
+        \DB::table('languages')->insert(array(
+            0 =>
+                array(
+                    'id' => 1,
+                    'local' => 'en',
+                    'name' => 'English',
+                    'def' => 1,
+                    'created_at' => '2020-01-21 12:08:34',
+                    'updated_at' => '2020-01-21 12:08:34',
+                ),
+            1 =>
+                array(
+                    'id' => 2,
+                    'local' => 'ar',
+                    'name' => 'العربية',
+                    'def' => 0,
+                    'created_at' => '2020-09-05 12:08:34',
+                    'updated_at' => '2020-09-05 12:08:34',
+
+                ),
+        ));
+
+
+    }
+}
